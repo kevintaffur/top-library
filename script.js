b37544086d765b1266da9c088b813a2f14147af8
Kevin <86487929+kevintaffur@users.noreply.github.com>
2024-05-21 11:56:53 -0500

    add show and close form functionality

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const myLibrary = [];
 myLibrary[0] = new Book("Kevin", "This is an awesome book!", 321, true);
 myLibrary[1] = new Book("Gus", "Also a great book!", 200, false);
 
+const content = document.querySelector(".container .content");
+const bookForm = document.querySelector(".content .book-form");
+const newBookButton = document.querySelector("#new-button");
 
 function Book(author, title, numberOfPages, wasRead) {
   this.author = author;
@@ -23,3 +26,16 @@ function printBooks() {
     console.log(book);
   }
 }
+
+newBookButton.addEventListener("click", (e) => {
+  const mode = getComputedStyle(bookForm).getPropertyValue("display");
+  if (mode === "grid") {
+    bookForm.style.display = "none";
+    content.style.gridTemplateColumns = "1fr";
+    newBookButton.textContent = "NEW BOOK";
+  } else if (mode === "none") {
+    bookForm.style.display = "grid";
+    content.style.gridTemplateColumns = "2fr 1fr";
+    newBookButton.textContent = "CLOSE";
+  }
+});
