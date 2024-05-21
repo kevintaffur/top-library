c18eaa2105628b4a84aeb1d2675c010c74f14e70
Kevin <86487929+kevintaffur@users.noreply.github.com>
2024-05-21 12:56:05 -0500

    add new book functionality

--- script.js
+++ script.js
@@ -5,6 +5,15 @@ myLibrary[1] = new Book("Gus", "Also a great book!", 200, false);
 const content = document.querySelector(".container .content");
 const bookForm = document.querySelector(".content .book-form");
 const newBookButton = document.querySelector("#new-button");
+const addBookButton = document.querySelector("#add-button");
+
+// form data
+const title = document.querySelector("#title");
+const author = document.querySelector("#author");
+const numberOfPages = document.querySelector("#numberofpages");
+const wasRead = document.querySelector("#wasread");
+
+let checkRadioButton = false;
 
 function Book(author, title, numberOfPages, wasRead) {
   this.author = author;
@@ -21,13 +30,32 @@ function addBookToLibrary(book) {
   myLibrary.push(book);
 }
 
+function validateFields(titleField, authorField, pagesField, wasReadField) {
+  const title = titleField.trim();
+  const author = authorField.trim();
+  const pages = parseInt(pagesField);
+  const wasRead = wasReadField;
+
+  if (title === "" || author === "" || (isNaN(pages) || pages === 0)) {
+    return null;
+  }
+  return new Book(title, author, pages, wasRead);
+}
+
+function clearFields() {
+  title.value = "";
+  author.value = "";
+  numberOfPages.value = "";
+  wasRead.checked = false;
+}
+
 function printBooks() {
   for (const book of myLibrary) {
     console.log(book);
   }
 }
 
-newBookButton.addEventListener("click", (e) => {
+newBookButton.addEventListener("click", () => {
   const mode = getComputedStyle(bookForm).getPropertyValue("display");
   if (mode === "grid") {
     bookForm.style.display = "none";
@@ -38,4 +66,22 @@ newBookButton.addEventListener("click", (e) => {
     content.style.gridTemplateColumns = "2fr 1fr";
     newBookButton.textContent = "CLOSE";
   }
+});
+
+addBookButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  const book = validateFields(title.value, author.value, numberOfPages.value, wasRead.checked);
+
+  if (book) {
+    addBookToLibrary(book);
+    clearFields();
+  }
+  console.log(myLibrary);
+});
+
+wasRead.addEventListener("click", () => {
+  if (checkRadioButton) {
+    wasRead.checked = false;
+  }
+  checkRadioButton = !checkRadioButton;
 });
